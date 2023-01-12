a5e804a79695860f5cdb64658d5c462bcf8eefaf
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-01-12 18:19:56 +0100

    chore: change-releaserc-config

--- .releaserc.js
+++ .releaserc.js
@@ -1,11 +1,7 @@
 /** @type {import("semantic-release").Options} */
 const config = {
     extends: "@socialgouv/releaserc",
-    branches: ["main"],
-    plugins: [
-        "@semantic-release/changelog",
-        "@semantic-release/git"
-    ]
+    branches: ["main"]
 };
 
 module.exports = config;
