17d30374f35060364940bb7a9f7306e7d6ab73a9
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-01-25 00:55:44 +0100

    feat: change previous questions on diag

--- apps/web/src/design-system/base/FormSelect.tsx
+++ apps/web/src/design-system/base/FormSelect.tsx
@@ -6,11 +6,15 @@ export type FormSelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
   isDisabled?: boolean;
   isError?: boolean;
   isValid?: boolean;
+  placeholderHidden?: boolean;
   placeholderSelected?: boolean;
 };
 
 export const FormSelect = forwardRef<HTMLSelectElement, FormSelectProps>(
-  ({ placeholder, className, isDisabled, isValid, isError, children, placeholderSelected, ...rest }, ref) => {
+  (
+    { placeholder, className, isDisabled, isValid, isError, children, placeholderSelected, placeholderHidden, ...rest },
+    ref,
+  ) => {
     return (
       <select
         className={clsx("fr-select", isError && "fr-select--error", isValid && "fr-select--valid", className)}
@@ -20,7 +24,7 @@ export const FormSelect = forwardRef<HTMLSelectElement, FormSelectProps>(
         {...(placeholderSelected ? { defaultValue: "" } : {})}
       >
         {placeholder && (
-          <option value="" disabled hidden>
+          <option value="" disabled hidden={placeholderHidden}>
             {placeholder}
           </option>
         )}
