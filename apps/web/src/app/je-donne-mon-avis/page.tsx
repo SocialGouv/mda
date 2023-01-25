7941e00e92d376a61d23585a7e6f7703543bdde4
laurent <laurent@sutterlity.studio>
2023-01-25 15:31:56 +0100

    feat: update form content

--- apps/web/src/app/je-donne-mon-avis/page.tsx
+++ apps/web/src/app/je-donne-mon-avis/page.tsx
@@ -1,4 +1,5 @@
 import { SimpleContentPage } from "@components/base/SimpleContentPage";
+import { Alert, AlertTitle } from "@design-system";
 
 import { FeedbackForm } from "./FeedbackForm";
 
@@ -7,10 +8,23 @@ const Feedback = () => {
     <SimpleContentPage>
       <h1>Partagez votre avis sur ce site internet et vos idées d'amélioration.</h1>
       <p className="fr-text--lg">
-        Ce site est fait pour les personnes autistes, par des personnes autistes, et avec des personnes autistes. Les
-        commentaires, réactions et propositions des personnes concernées (personnes autistes, parents, aidants,
-        professionnels...) sont précieux pour nous aider à améliorer ce service.
+        Ce site est fait pour les personnes autistes, par des personnes autistes, et avec des personnes concernées.
+        C'est une version beta en cours de déploiement progressif. Vos commentaires, réactions et propositions sont
+        précieux pour nous aider à améliorer ce service au fur et à mesure.
       </p>
+      <Alert>
+        <AlertTitle as="h2">
+          Attention, les demandes personnelles transmises via ce formulaire ne pourront être traitées sur ce site.
+        </AlertTitle>
+        <p>
+          Pour les demandes personnelles, veuillez vous rapprocher de l'organisme en charge de votre dossier. Pour toute
+          question, vous pouvez contacter{" "}
+          <a href="https://www.autismeinfoservice.fr/" target="_blank" rel="noreferrer">
+            Autisme Info Services
+          </a>
+          .
+        </p>
+      </Alert>
       <FeedbackForm />
     </SimpleContentPage>
   );
