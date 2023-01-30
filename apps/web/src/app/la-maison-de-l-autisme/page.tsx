6e0af8d9ca1b373515ca6433741546ef065272d4
Sutterlity Laurent <laurent@sutterlity.studio>
2023-01-30 17:37:33 +0100

    feat: add wording and links (#75) (#77)

--- apps/web/src/app/la-maison-de-l-autisme/page.tsx
+++ apps/web/src/app/la-maison-de-l-autisme/page.tsx
@@ -9,11 +9,49 @@ const AutismHouse = () => {
       <ActionsButtons />
       <h1>La Maison de l'autisme</h1>
       <p className="fr-text--xl">
-        La Maison de l’autisme n’est plus un projet mais une réalité. En cours de construction avec ses futurs usagers,
-        elle ouvrira ses portes physiques et numériques au 2ème trimestre 2023.
+        La Maison de l’autisme n’est plus un projet, c’est une réalité. Elle se poursuit avec vous. Elle a hâte de vous
+        accueillir à Aubervilliers et en ligne dès avril prochain.
       </p>
       <CollapsedSectionDynamicGroup
         data={[
+          {
+            content: (
+              <>
+                <p>La Maison physique de l’autisme sera accessible par métro, bus, RER et train.</p>
+                <p>
+                  Elle sera située à 5 minutes à pied de la station Front populaire sur la ligne 12 du métro parisien et
+                  à 10 minutes à pied de la station de RER “la Plaine Stade de France”.
+                </p>
+                <p>
+                  Elle sera située à 30 minutes en métro des gares parisiennes (Nord, Saint-Lazare, Est, Montparnasse,
+                  Lyon, Austerlitz) en moyenne.
+                </p>
+                <p>
+                  La station de métro Front Populaire est facile d’accès, en ce qu’elle est équipée d'escalators et
+                  d'ascenseurs sur tout le parcours entre la rue et le quai. Elle bénéficie de la lumière du jour et
+                  d’une température ambiante toute l’année.
+                </p>
+                <p>
+                  Un travail sur la signalétique et l’aménagement urbain est en cours pour que tous les usagers accèdent
+                  très facilement à la Maison de l’autisme depuis leur domicile ou lieu de travail (Maison de l’autisme
+                  repérable dès la sortie du métro, cheminement lisible et sans obstacle entre le métro et l’enceinte de
+                  la Maison de l’autisme…).
+                </p>
+                <p>
+                  La Maison numérique de l’autisme sera accessible 24h/24 7j/7 via le site internet. Elle permettra aux
+                  usagers d’anticiper leur venue sur place grâce à une vidéo expliquant l’accès.
+                </p>
+                <p>
+                  <ButtonAsLink href="https://goo.gl/maps/2CjQCW9Nku9Q87da9" target="_blank" rel="noreferrer">
+                    Voir dans Google Maps
+                  </ButtonAsLink>
+                </p>
+              </>
+            ),
+            id: "comment-pourrai-je-me-rendre-a-la-maison-de-lautisme",
+            title: "Comment pourrai-je me rendre à la Maison de l’autisme ?",
+          },
+
           {
             content: (
               <>
@@ -38,23 +76,7 @@ const AutismHouse = () => {
             id: "quelles-sont-les-missions-de-la-maison-de-lautisme",
             title: "Quelles sont les missions de la Maison de l’autisme ?",
           },
-          {
-            content: (
-              <>
-                <p>
-                  Des études d’impact de la stratégie nationale autisme menées entre 2019 et 2021 ont soulevé un déficit
-                  d’informations fiables à destination des personnes autistes et de leurs proches (familles, aidants…).
-                </p>
-                <p>
-                  Lors de la Conférence Nationale du Handicap du 11 février 2020, le Président de la République a
-                  annoncé la création d’une “maison de l’autisme” comme lieu de ressources, de rencontres et
-                  d’accompagnement des personnes dans l’accès et l’exercice de leurs droits.
-                </p>
-              </>
-            ),
-            id: "quelle-est-lhistoire-de-la-maison-de-lautisme",
-            title: "Quelle est l’histoire de la Maison de l’autisme ? ",
-          },
+
           {
             content: (
               <>
@@ -84,64 +106,6 @@ const AutismHouse = () => {
             ),
             id: "a-quoi-sert-la-maison-de-lautisme-quel-interet-dy-aller",
             title: " À quoi sert la Maison de l’autisme ? Quel intérêt d'y aller ?",
-          },
-          {
-            content: (
-              <>
-                <p>
-                  La Maison de l'autisme sera dans un premier temps co-animée par le Groupement National des Centres de
-                  Ressources Autisme, le Centre de Ressources Autisme en Île-de-France et Autisme Info Service, puis par
-                  d’autres associations.
-                </p>
-                <p>
-                  Pour plus d’information et contacter ces acteurs en direct, rendez-vous sur leurs sites
-                  internet&nbsp;:
-                </p>
-                <ul>
-                  <li>
-                    <a href="https://www.autismeinfoservice.fr/" target="_blank" rel="noreferrer">
-                      Autisme Info Service
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.autismeinfoservice.fr/" target="_blank" rel="noreferrer">
-                      https://www.craif.org/
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://gncra.fr/" target="_blank" rel="noreferrer">
-                      Groupement National des Centres Ressources Autisme
-                    </a>
-                  </li>
-                </ul>
-              </>
-            ),
-            id: "quels-acteurs-composeront-la-maison-de-lautisme",
-            title: "Quels acteurs composeront la Maison de l’autisme ?",
-          },
-          {
-            content: (
-              <>
-                <p>
-                  Il sera naturellement possible de se rendre à la Maison de l’autisme avec son enfant ou son
-                  adolescent.
-                </p>
-                <p>
-                  Pendant que le parent ou l’aidant sera en rendez-vous ou participera à un événement de la
-                  programmation (visionnage d’une projection, rencontre avec un auteur…), l’enfant ou l’adolescent
-                  disposera d’outils, d’équipements et de services : dans un dispositif sensoriel avec des contenus
-                  pédagogiques, il pourra se ressourcer en sécurité, en silence ou en immersion dans des univers
-                  apaisants et stimulants ou rejoindre un groupe de lecture dans d’excellentes conditions acoustiques.
-                </p>
-                <p>Il pourra grimper en escalade en sécurité sur un mur dédié. </p>
-                <p>
-                  Il pourra jouer d’instruments de musique sans importuner les autres usagers, en sécurité pour lui et
-                  pour les autres et sans risque d’endommager le matériel.
-                </p>
-              </>
-            ),
-            id: "est-ce-que-je-pourrai-y-aller-avec-mon-enfant-qui-pourra-le-garder-sur-place",
-            title: "Est-ce que je pourrai y aller avec mon enfant ? Qui pourra le garder sur place ?",
           },
           {
             content: (
@@ -209,39 +173,77 @@ const AutismHouse = () => {
           {
             content: (
               <>
-                <p>La Maison physique de l’autisme sera accessible par métro, bus, RER et train.</p>
                 <p>
-                  Elle sera située à 5 minutes à pied de la station Front populaire sur la ligne 12 du métro parisien et
-                  à 10 minutes à pied de la station de RER “la Plaine Stade de France”.
+                  Il sera naturellement possible de se rendre à la Maison de l’autisme avec son enfant ou son
+                  adolescent.
                 </p>
                 <p>
-                  Elle sera située à 30 minutes en métro des gares parisiennes (Nord, Saint-Lazare, Est, Montparnasse,
-                  Lyon, Austerlitz) en moyenne.
+                  Pendant que le parent ou l’aidant sera en rendez-vous ou participera à un événement de la
+                  programmation (visionnage d’une projection, rencontre avec un auteur…), l’enfant ou l’adolescent
+                  disposera d’outils, d’équipements et de services : dans un dispositif sensoriel avec des contenus
+                  pédagogiques, il pourra se ressourcer en sécurité, en silence ou en immersion dans des univers
+                  apaisants et stimulants ou rejoindre un groupe de lecture dans d’excellentes conditions acoustiques.
                 </p>
+                <p>Il pourra grimper en escalade en sécurité sur un mur dédié. </p>
                 <p>
-                  La station de métro Front Populaire est facile d’accès, en ce qu’elle est équipée d'escalators et
-                  d'ascenseurs sur tout le parcours entre la rue et le quai. Elle bénéficie de la lumière du jour et
-                  d’une température ambiante toute l’année.
-                </p>
-                <p>
-                  Un travail sur la signalétique et l’aménagement urbain est en cours pour que tous les usagers accèdent
-                  très facilement à la Maison de l’autisme depuis leur domicile ou lieu de travail (Maison de l’autisme
-                  repérable dès la sortie du métro, cheminement lisible et sans obstacle entre le métro et l’enceinte de
-                  la Maison de l’autisme…).
-                </p>
-                <p>
-                  La Maison numérique de l’autisme sera accessible 24h/24 7j/7 via le site internet. Elle permettra aux
-                  usagers d’anticiper leur venue sur place grâce à une vidéo expliquant l’accès.
-                </p>
-                <p>
-                  <ButtonAsLink href="https://goo.gl/maps/2CjQCW9Nku9Q87da9" target="_blank" rel="noreferrer">
-                    Voir dans Google Maps
-                  </ButtonAsLink>
+                  Il pourra jouer d’instruments de musique sans importuner les autres usagers, en sécurité pour lui et
+                  pour les autres et sans risque d’endommager le matériel.
                 </p>
               </>
             ),
-            id: "comment-pourrai-je-me-rendre-a-la-maison-de-lautisme",
-            title: "Comment pourrai-je me rendre à la Maison de l’autisme ?",
+            id: "est-ce-que-je-pourrai-y-aller-avec-mon-enfant-qui-pourra-le-garder-sur-place",
+            title: "Est-ce que je pourrai y aller avec mon enfant ? Qui pourra le garder sur place ?",
+          },
+          {
+            content: (
+              <>
+                <p>
+                  La Maison de l'autisme sera dans un premier temps co-animée par le Groupement National des Centres de
+                  Ressources Autisme, le Centre de Ressources Autisme en Île-de-France et Autisme Info Service, puis par
+                  d’autres associations.
+                </p>
+                <p>
+                  Pour plus d’information et contacter ces acteurs en direct, rendez-vous sur leurs sites
+                  internet&nbsp;:
+                </p>
+                <ul>
+                  <li>
+                    <a href="https://www.autismeinfoservice.fr/" target="_blank" rel="noreferrer">
+                      Autisme Info Service
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.autismeinfoservice.fr/" target="_blank" rel="noreferrer">
+                      https://www.craif.org/
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://gncra.fr/" target="_blank" rel="noreferrer">
+                      Groupement National des Centres Ressources Autisme
+                    </a>
+                  </li>
+                </ul>
+              </>
+            ),
+            id: "quels-acteurs-composeront-la-maison-de-lautisme",
+            title: "Quels acteurs composeront la Maison de l’autisme ?",
+          },
+          {
+            content: (
+              <>
+                <p>
+                  Des études d’impact de la stratégie nationale autisme menées entre 2019 et 2021 ont soulevé un déficit
+                  d’informations fiables à destination des personnes autistes et de leurs proches (familles, aidants…).
+                </p>
+                <p>
+                  Lors de la Conférence Nationale du Handicap du 11 février 2020, le Président de la République a
+                  annoncé la création d’une “maison de l’autisme” comme lieu de ressources, de rencontres et
+                  d’accompagnement des personnes dans l’accès et l’exercice de leurs droits.
+                </p>
+              </>
+            ),
+            id: "quelle-est-lhistoire-de-la-maison-de-lautisme",
+            title: "Quelle est l’histoire de la Maison de l’autisme ? ",
           },
         ]}
       />
