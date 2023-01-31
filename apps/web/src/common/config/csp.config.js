c56d29ec8d4454130cabd946e7b33e615792f88c
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-01-31 19:04:42 +0100

    fix: csp default src

--- apps/web/src/common/config/csp.config.js
+++ apps/web/src/common/config/csp.config.js
@@ -1,9 +1,9 @@
 // TODO change for actual domain
 // TODO change to strict CSP or nonce with predefined value
 const ContentSecurityPolicy = `
-  default-src 'self' https://*.gouv.fr;
+  default-src 'self' https://*.gouv.fr https://unpkg.com;
   img-src 'self' data: https://*.gouv.fr https://www.googletagmanager.com;
-  script-src 'self' https://*.gouv.fr https://www.googletagmanager.com https://unpkg.com 'unsafe-inline' 'unsafe-eval';
+  script-src 'self' https://*.gouv.fr https://www.googletagmanager.com 'unsafe-inline' 'unsafe-eval';
   frame-src 'self' https://*.gouv.fr;
   style-src 'self' 'unsafe-inline';
   font-src 'self' data: blob:;
