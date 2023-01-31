75acaa5ab1f8aabd975d6f66b9df2ed535464064
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-01-31 19:38:11 +0100

    fix: widen csp

--- apps/web/src/common/config/csp.config.js
+++ apps/web/src/common/config/csp.config.js
@@ -1,12 +1,12 @@
 // TODO change for actual domain
 // TODO change to strict CSP or nonce with predefined value
 const ContentSecurityPolicy = `
-  default-src 'self' https://*.gouv.fr https://unpkg.com;
-  img-src 'self' data: https://*.gouv.fr https://www.googletagmanager.com;
-  script-src 'self' https://*.gouv.fr https://www.googletagmanager.com 'unsafe-inline' 'unsafe-eval';
+  default-src 'self' https://*.gouv.fr;
+  img-src 'self' data: https://*.gouv.fr https://www.googletagmanager.com https://unpkg.com;
+  script-src 'self' https://*.gouv.fr https://www.googletagmanager.com https://unpkg.com 'unsafe-inline' 'unsafe-eval';
   frame-src 'self' https://*.gouv.fr;
-  style-src 'self' 'unsafe-inline';
-  font-src 'self' data: blob:;
+  style-src 'self' https://unpkg.com 'unsafe-inline';
+  font-src 'self' data: blob: https://unpkg.com;
 `
   .replace(/\n/g, " ")
   .trim();
