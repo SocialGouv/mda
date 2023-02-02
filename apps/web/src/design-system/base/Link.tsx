03a79a092357875e7f8e0e36de0801ef2a742a57
laurent <laurent@sutterlity.studio>
2023-02-02 16:48:40 +0100

    fix: lint

--- apps/web/src/design-system/base/Link.tsx
+++ apps/web/src/design-system/base/Link.tsx
@@ -1,42 +1,49 @@
 import clsx from "clsx";
-import { forwardRef } from "react";
+import { type PropsWithChildren } from "react";
 
 import type { IconStyles } from "../utils/IconStyles";
 import { NextLinkOrA } from "../utils/NextLinkOrA";
 
-export type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+export type LinkProps = {
+  href?: string;
   iconLeft?: IconStyles;
   iconRight?: IconStyles;
   isCurrent?: boolean;
   isDisabled?: boolean;
+  rel?: string;
   size?: "lg" | "sm";
+  target?: string;
+  title?: string;
 };
 
-export type LinkRef = HTMLAnchorElement;
-
-export const Link = forwardRef<LinkRef, LinkProps>(
-  ({ href, children, size, iconLeft, iconRight, target, isDisabled, isCurrent, ...rest }, ref) => {
-    return (
-      <NextLinkOrA
-        ref={ref}
-        href={href || undefined}
-        aria-current={isCurrent ? "page" : undefined}
-        aria-disabled={isDisabled || !href ? true : undefined}
-        className={clsx(
-          "fr-link",
-          size === "sm" && "fr-link--sm",
-          size === "lg" && "fr-link--lg",
-          iconLeft && `fr-link--icon-left ${iconLeft}`,
-          iconRight && `fr-link--icon-right ${iconRight}`,
-        )}
-        target={target}
-        rel={target === "_blank" ? "noopener noreferrer" : undefined}
-        {...rest}
-      >
-        {children}
-      </NextLinkOrA>
-    );
-  },
+export const Link = ({
+  href,
+  children,
+  size,
+  iconLeft,
+  iconRight,
+  target,
+  isDisabled,
+  isCurrent,
+  ...rest
+}: PropsWithChildren<LinkProps>) => (
+  <NextLinkOrA
+    href={href || undefined}
+    aria-current={isCurrent ? "page" : undefined}
+    aria-disabled={isDisabled || !href ? true : undefined}
+    className={clsx(
+      "fr-link",
+      size === "sm" && "fr-link--sm",
+      size === "lg" && "fr-link--lg",
+      iconLeft && `fr-link--icon-left ${iconLeft}`,
+      iconRight && `fr-link--icon-right ${iconRight}`,
+    )}
+    target={target}
+    rel={target === "_blank" ? "noopener noreferrer" : undefined}
+    {...rest}
+  >
+    {children}
+  </NextLinkOrA>
 );
 
 Link.displayName = "Link";
