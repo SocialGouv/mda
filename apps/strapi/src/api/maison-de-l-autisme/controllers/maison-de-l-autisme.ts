06dde10743db10c28b7a487fb9ec640b9c75db67
laurent <laurent@sutterlity.studio>
2023-02-16 17:34:47 +0100

    feat: add editable Maison de l'autisme page on Strapi

---
+++ apps/strapi/src/api/maison-de-l-autisme/controllers/maison-de-l-autisme.ts
@@ -0,0 +1,7 @@
+/**
+ * maison-de-l-autisme controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::maison-de-l-autisme.maison-de-l-autisme');
