97b561e5ea600a4162f0bf0e08ac2230d5e05449
Laurent Sutterlity <laurent@sutterlity.studio>
2023-03-07 16:59:34 +0100

    fix: typo

--- apps/web/src/app/(main)/mon-diagnostic/head.tsx
+++ apps/web/src/app/(main)/mon-diagnostic/head.tsx
@@ -1,7 +1,7 @@
 import { Next13Seo } from "@components/utils/Next13Seo";
 
 const Head = () => {
-  return <Next13Seo title="Mon diagnostique" />;
+  return <Next13Seo title="Mon diagnostic" />;
 };
 
 export default Head;
