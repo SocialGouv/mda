409cce5f8d0843c6a9f9c0c0d84de01f29b34e8c
Paul Souche <paul.souche@gmail.com>
2023-03-14 16:59:40 +0100

    fix(videos): Allow embedded youtube videos

--- apps/web/src/common/config/csp.config.js
+++ apps/web/src/common/config/csp.config.js
@@ -4,7 +4,7 @@ const ContentSecurityPolicy = `
   default-src 'self' https://*.gouv.fr;
   img-src 'self' data: https://*.gouv.fr https://www.googletagmanager.com https://unpkg.com;
   script-src 'self' https://*.gouv.fr https://www.googletagmanager.com https://unpkg.com 'unsafe-inline' 'unsafe-eval';
-  frame-src 'self' https://*.gouv.fr;
+  frame-src 'self' https://*.gouv.fr https://www.youtube.com;
   style-src 'self' https://unpkg.com 'unsafe-inline';
   font-src 'self' data: blob: https://unpkg.com;
 `
