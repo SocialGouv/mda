1b9b86c4ecb7f82b5e86b3650da1bc2cc1cc22b6
Laurent Sutterlity <laurent@sutterlity.studio>
2023-03-27 10:42:00 +0200

    feat: activating Matomo session time calculation

--- apps/web/src/components/utils/client/Matomo.tsx
+++ apps/web/src/components/utils/client/Matomo.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { config } from "@common/config";
-import { init } from "@socialgouv/matomo-next";
+import { init, push } from "@socialgouv/matomo-next";
 import { useEffect } from "react";
 
 export type MatomoProps = Pick<typeof config, "env">;
 
 export const Matomo = ({ env }: MatomoProps) => {
   useEffect(() => {
-    env === "prod" && init(config.matomo);
+    env === "prod" &&
+      init({
+        ...config.matomo,
+        onInitialization: () => {
+          push(["enableHeartBeatTimer"]);
+        },
+      });
   }, [env]);
 
   return <></>;
