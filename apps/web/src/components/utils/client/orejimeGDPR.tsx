b46fd86cf8b085a786caf56d1a9237e4191acf61
Paul Souche <paul.souche@gmail.com>
2023-03-31 14:08:13 +0200

    wip

--- apps/web/src/components/utils/client/orejimeGDPR.tsx
+++ apps/web/src/components/utils/client/orejimeGDPR.tsx
@@ -7,25 +7,35 @@ export const OrejimeGDPR = () => {
   const handleLoad: ScriptProps["onLoad"] = useCallback(() => {
     const orejime = Orejime.init({
       privacyPolicy: "/politique-de-confidentialite#cookies",
+      cookieExpiresAfterDays: 182,
+      debug: false,
+      mustConsent: false,
+      mustNotice: false,
+      implicitConsent: false,
+      translations: {
+        fr: {
+          poweredBy: false,
+          consentNotice: {
+            description:
+              "Ce site utilise des cookies permettant de visualiser des contenus provenant des réseaux sociaux et d'améliorer le fonctionnement grâce aux statistiques de navigation. Votre choix est conservé pendant 6 mois et vous pouvez être informé et modifier vos préférences à tout moment sur la page « Politique de confidentialité ».",
+            learnMore: "Politique de confidentialité",
+          },
+        },
+      },
       appElement: "#orejime",
       apps: [
         {
           name: "internal-tracker",
           title: "Cookies techniques nécessaires au bon fonctionnement du site",
-          purposes: ["Analytics"],
           description:
             "Utilisation: Ces cookies permettent au site de fonctionner de manière optimale. Vous pouvez vous y opposer et les supprimer en utilisant les paramètres de votre navigateur, cependant votre expérience utilisateur risque d’être dégradée.",
-          required: !0,
+          required: true,
         },
         {
           name: "streaming-video",
           title: "Streaming Vidéo",
-          purposes: ["Streaming vidéo"],
           description:
             'Utilisation: Ces cookies permettent la lecture de vidéos sur le site maisondelautisme en provenance de sites d’hébergement de contenus. <a href="https://policies.google.com/privacy" target="_blank" title="politique de confidentialité de YouTube - nouvelle fenêtre">"Politique de confidentialité de YouTube"</a>',
-          callback: function (...args: unknown[]) {
-            console.log(args);
-          },
           optOut: false,
           default: false,
           onlyOnce: false,
