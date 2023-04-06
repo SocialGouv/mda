27204ba71e1967e8103aae99d5389a481c3c3a5b
Paul Souche <paul.souche@gmail.com>
2023-04-06 16:35:05 +0200

    fix(search): Use server component

--- apps/web/src/common/utils/next13.ts
+++ apps/web/src/common/utils/next13.ts
@@ -1,5 +1,8 @@
-export interface Next13ServerPageProps<Param extends string> {
+export interface Next13ServerPageProps<Param extends string = "", SearchParams extends string = ""> {
   params: {
     [T in Param]: string;
+  };
+  searchParams: {
+    [T in SearchParams]: string;
   };
 }
