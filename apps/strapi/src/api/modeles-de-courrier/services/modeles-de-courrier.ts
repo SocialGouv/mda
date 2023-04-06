95e477812aea4fe7c9d68886b1aae35a46ece26f
Paul Souche <paul.souche@gmail.com>
2023-04-06 12:17:01 +0200

    feat(modeles-de-courrier): dynamisation (#243)

---
+++ apps/strapi/src/api/modeles-de-courrier/services/modeles-de-courrier.ts
@@ -0,0 +1,7 @@
+/**
+ * modeles-de-courrier service
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreService("api::modeles-de-courrier.modeles-de-courrier");
