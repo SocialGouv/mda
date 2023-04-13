d77bcb41b960d315fa05b109c72c09475146c376
Paul Souche <paul.souche@gmail.com>
2023-04-13 17:41:41 +0200

    feat(pictos): Add all DSFR pictos

--- apps/web/src/components/home/GridTiles.tsx
+++ apps/web/src/components/home/GridTiles.tsx
@@ -1,20 +1,11 @@
-import { PictoDocumentDownload } from "@components/pictos/PictoDocumentDownload";
-import { PictoHealth } from "@components/pictos/PictoHealth";
-import { PictoHumanCooperation } from "@components/pictos/PictoHumanCooperation";
-import { PictoMap } from "@components/pictos/PictoMap";
 import { Container, Grid, GridCol, Tile, TileBody, TileBodyDescription, TileBodyTitle, TileImg } from "@design-system";
 import { type GetAttributesValues } from "@mda/strapi-types";
+
+import { PICTOS } from "../pictos";
 
 interface GridTilesProps {
   grid: GetAttributesValues<"common.grid-tiles">;
 }
-
-const TILES_PICTO = {
-  documentDownload: <PictoDocumentDownload />,
-  health: <PictoHealth />,
-  humanCooperation: <PictoHumanCooperation />,
-  map: <PictoMap />,
-};
 
 export const GridTiles = ({ grid }: GridTilesProps) => (
   <div className="fr-pt-6w fr-pt-md-8w">
@@ -31,7 +22,7 @@ export const GridTiles = ({ grid }: GridTilesProps) => (
                   </TileBodyTitle>
                   <TileBodyDescription>{tile.description}</TileBodyDescription>
                 </TileBody>
-                <TileImg>{TILES_PICTO[tile.picto]}</TileImg>
+                <TileImg>{PICTOS[tile.picto]}</TileImg>
               </Tile>
             </GridCol>
           ))}
