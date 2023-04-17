04490c0f0cdcf1c73079e1de81ab4b1ef107ae25
Paul Souche <paul.souche@gmail.com>
2023-04-17 12:26:08 +0200

    fix(matomo): Track navigation

--- apps/web/src/components/utils/client/Matomo.tsx
+++ apps/web/src/components/utils/client/Matomo.tsx
@@ -3,23 +3,29 @@
 import { config } from "@common/config";
 import { useGdprStore } from "@design-system/client";
 import { init, push } from "@socialgouv/matomo-next";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export type MatomoProps = Pick<typeof config, "env">;
 
 export const Matomo = ({ env }: MatomoProps) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   const matomoConsent = useGdprStore(state => state.consents.matomo);
   const [inited, setInited] = useState(false);
+  const [previousPath, setPreviousPath] = useState("");
 
   useEffect(() => {
     if (env === "prod" && !inited) {
       init({
         ...config.matomo,
         onInitialization: () => {
-          push(["enableHeartBeatTimer"]);
           push(["requireCookieConsent"]);
+          push(["enableHeartBeatTimer"]);
+          push(["disableQueueRequest"]);
         },
       });
+
       setInited(true);
     }
 
@@ -31,6 +37,40 @@ export const Matomo = ({ env }: MatomoProps) => {
       push(["forgetCookieConsentGiven"]);
     }
   }, [env, inited, matomoConsent]);
+
+  /**
+   * The @socialgouv/matomo-next does not work with next 13
+   */
+  useEffect(() => {
+    if (!pathname || !matomoConsent) {
+      return;
+    }
+
+    if (!previousPath) {
+      return setPreviousPath(pathname);
+    }
+
+    push(["setReferrerUrl", `${previousPath}`]);
+    push(["setCustomUrl", pathname]);
+    push(["deleteCustomVariables", "page"]);
+    setPreviousPath(pathname);
+    // In order to ensure that the page title had been updated,
+    // we delayed pushing the tracking to the next tick.
+    setTimeout(() => {
+      push(["setDocumentTitle", document.title]);
+      if (pathname.startsWith("/recherche")) {
+        push(["trackSiteSearch", searchParams?.get("keyword") ?? ""]);
+      } else {
+        push(["trackPageView"]);
+      }
+    });
+    /**
+     * This is because we don't want to track previousPath
+     * could be a if (previousPath === pathname) return; instead
+     * But be sure to not send the tracking twice
+     */
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [matomoConsent, pathname, searchParams]);
 
   return <></>;
 };
