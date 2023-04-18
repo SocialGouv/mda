50aaa69586c5062d2ef61fda21759de9b87dd729
Paul Souche <paul.souche@gmail.com>
2023-04-18 21:55:40 +0200

    fix(sitemap): remove healthz

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 const sitemapConfig = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://maisondelautisme.fabrique.social.gouv.fr/",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://maisondelautisme.gouv.fr/",
   generateRobotsTxt: process.env.ROBOT === "true",
+  exclude: ["/healthz", "/recherche"],
 };
 
 module.exports = sitemapConfig;
