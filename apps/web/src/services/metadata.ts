f309bc2fbd2db120687accefe30f6122f4556ffe
Paul Souche <paul.souche@gmail.com>
2023-04-18 15:01:29 +0200

    feat(seo): Add keywords

--- apps/web/src/services/metadata.ts
+++ apps/web/src/services/metadata.ts
@@ -2,7 +2,19 @@ import { config } from "@common/config";
 import { type Metadata, type ResolvingMetadata } from "next";
 import { type TemplateString } from "next/dist/lib/metadata/types/metadata-types";
 
+const DEFAULT_KEYWORDS = [
+  "autisme",
+  "troubles du spectre de l'autisme",
+  "TSA",
+  "TND",
+  "Troubles du neurodéveloppement",
+  "diagnostic, TDA/H",
+  "asperger",
+  "trouble autistique",
+];
+
 interface ResolvedMetadatas {
+  keywords?: Metadata["keywords"];
   slug?: string;
   title: TemplateString | string;
 }
@@ -15,12 +27,13 @@ interface MetadataFactoryParams {
 export const generateMetadataFactory =
   ({ noCanonicalLink, resolveMetadata }: MetadataFactoryParams) =>
   async (params: unknown, parent: ResolvingMetadata): Promise<Metadata> => {
-    const { title, slug } = await resolveMetadata(params);
+    const { keywords, title, slug } = await resolveMetadata(params);
     const { alternates } = await parent;
     const baseUrl = alternates?.canonical?.url.toString() || config.siteUrl;
 
     return {
       title,
+      keywords: keywords ?? DEFAULT_KEYWORDS,
       alternates: noCanonicalLink
         ? {}
         : {
