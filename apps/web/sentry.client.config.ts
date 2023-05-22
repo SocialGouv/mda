79235702ed205569967d5d914f183cec0fd9c559
Adrien Chauve <adrien@chauve.eu>
2023-05-22 17:15:25 +0200

    fix: add sentry with profiling

---
+++ apps/web/sentry.client.config.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || "https://6d1e3666677742c8ad4030497c7b9ab5@sentry.fabrique.social.gouv.fr/83",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
